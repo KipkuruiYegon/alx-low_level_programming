7d40204c003a814beb213981307fb610cc4290ca
KipkuruiYegon <kevinkipkurui1@gmail.com>
2023-08-14 13:52:36 -0700

    a function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dog
+ *
+ * @d: the dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
